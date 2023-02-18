0d4dd875daebac2b79f8d8d4005e93ed63c14d93
Zach <zbayard@gmail.com>
2023-02-18 09:22:26 -0500

    here we go

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='super-taste'>supertaste shit brah</div>
+    </div>
+  );
+}
+
+export default App;
